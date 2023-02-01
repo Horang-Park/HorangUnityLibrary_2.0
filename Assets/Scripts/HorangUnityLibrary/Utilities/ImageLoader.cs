9c62ec42c433abbcfbf46fe9ca495a8c1bae17ed
HorangPark <horang_park@naver.com>
2023-02-01 15:23:14 +0900

    이미지 로더 추가

---
+++ Assets/Scripts/HorangUnityLibrary/Utilities/ImageLoader.cs
@@ -0,0 +1,188 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace HorangUnityLibrary.Utilities
+{
+	public static class ImageLoader
+	{
+		private static readonly string[] SupportImageFileExtension = { "png", "jpg", "jpeg", "tga" };
+
+		private delegate UniTask<Sprite> TaskDelegate(string path);
+		private static TaskDelegate loadManyDelegate;
+
+		/// <summary>
+		/// Load sprite from local storage.
+		/// </summary>
+		/// <param name="path">To load path</param>
+		/// <returns>Async load sprite with UniTask</returns>
+		public static async UniTask<Sprite> LoadFromLocal(string path)
+		{
+			var extension = Path.GetExtension(path);
+
+			if (ValidationImageFileExtension(extension) is false)
+			{
+				Log.Print($"To load image path({path}) is missing file extension or not image file extension.\nSupport file extension is [png, jpg, jpeg, tga].", LogPriority.Error);
+
+				return null;
+			}
+
+			var fileInfo = ValidationFileExist(path);
+
+			if (fileInfo is null)
+			{
+				return null;
+			}
+
+			var textureFileStream = new FileStream(path, FileMode.Open);
+			var textureByteBuffer = new byte[textureFileStream.Length];
+			var loadBytes = await textureFileStream.ReadAsync(textureByteBuffer, 0, (int)textureFileStream.Length);
+			
+			Log.Print($"Load complete: {path}, Load bytes: {loadBytes}", LogPriority.Verbose);
+			
+			textureFileStream.Close();
+			await textureFileStream.DisposeAsync();
+
+			return ConvertByteTextureToSprite(textureByteBuffer);
+		}
+
+		/// <summary>
+		/// Load many sprites from local storage.
+		/// </summary>
+		/// <param name="paths">To load paths enumerable</param>
+		/// <returns>Async load sprites array with UniTask</returns>
+		public static async UniTask<Sprite[]> LoadManyFromLocal(IEnumerable<string> paths)
+		{
+			loadManyDelegate = LoadFromLocal;
+
+			return await UniTask.WhenAll(CreateImageLoadTasks(paths, loadManyDelegate));
+		}
+
+		/// <summary>
+		/// Load sprite from remote server.
+		/// </summary>
+		/// <param name="path">To load path from remote server</param>
+		/// <returns>Async load sprite with UniTask</returns>
+		public static async UniTask<Sprite> LoadFromRemote(string path)
+		{
+			var imageRequester = UnityWebRequestTexture.GetTexture(path);
+			
+			Log.Print($"Load start. URI: {imageRequester.uri.AbsoluteUri}, API method: {imageRequester.method}", LogPriority.Verbose);
+			
+			try
+			{
+				imageRequester = await imageRequester.SendWebRequest().ToUniTask();
+			}
+			catch (UnityWebRequestException e)
+			{
+				Log.Print($"Load failed. Response Code: {e.ResponseCode}, Message: {e.Message}, URI: {e.UnityWebRequest.uri}", LogPriority.Error);
+				
+				imageRequester.Dispose();
+
+				return null;
+			}
+			catch (Exception e)
+			{
+				Log.Print($"Load failed. HR: {e.HResult}, Message: {e.Message}", LogPriority.Error);
+				
+				imageRequester.Dispose();
+
+				return null;
+			}
+
+			var loadedSprite = ConvertByteTextureToSprite(imageRequester.downloadHandler.data);
+			
+			Log.Print($"Load complete. URI: {imageRequester.uri.AbsoluteUri}, Response Code: {imageRequester.responseCode}", LogPriority.Verbose);
+			
+			imageRequester.Dispose();
+
+			return loadedSprite;
+		}
+
+		/// <summary>
+		/// Load many sprites from remote server.
+		/// </summary>
+		/// <param name="paths">To load paths enumerable</param>
+		/// <returns>Async load sprites array with UniTask</returns>
+		public static async UniTask<Sprite[]> LoadManyFromRemote(IEnumerable<string> paths)
+		{
+			loadManyDelegate = LoadFromRemote;
+
+			return await UniTask.WhenAll(CreateImageLoadTasks(paths, loadManyDelegate));
+		}
+
+		/// <summary>
+		/// Load sprite from Unity internal resources folder.
+		/// </summary>
+		/// <param name="path">To load path from resources folder. beware not to add extension</param>
+		/// <returns>Async laod sprite with UniTask</returns>
+		public static async UniTask<Sprite> LoadFromResourcesFolder(string path)
+		{
+			if (await Resources.LoadAsync<Sprite>(path) is Sprite resourcesSprite)
+			{
+				Log.Print($"Load complete: {path}");
+				
+				return resourcesSprite;
+			}
+			
+			Log.Print($"The image in path({path}) is not exist.", LogPriority.Error);
+
+			return null;
+		}
+
+		/// <summary>
+		/// Load sprites from Unity internal resources folder.
+		/// </summary>
+		/// <param name="paths">To load paths enumerable</param>
+		/// <returns>Async load sprites array with UniTask</returns>
+		public static async UniTask<Sprite[]> LoadManyFromResourcesFolder(IEnumerable<string> paths)
+		{
+			loadManyDelegate = LoadFromResourcesFolder;
+			
+			return await UniTask.WhenAll(CreateImageLoadTasks(paths, loadManyDelegate));
+		}
+
+		private static bool ValidationImageFileExtension(string e)
+		{
+			if (string.IsNullOrEmpty(e) || string.IsNullOrWhiteSpace(e))
+			{
+				return false;
+			}
+
+			return SupportImageFileExtension.Contains(e);
+		}
+
+		private static FileInfo ValidationFileExist(string p)
+		{
+			var fileInfo = new FileInfo(p);
+
+			if (fileInfo.Exists)
+			{
+				return fileInfo;
+			}
+			
+			Log.Print($"To load file does not exist in path({p}).", LogPriority.Error);
+
+			return null;
+		}
+
+		private static Sprite ConvertByteTextureToSprite(byte[] btb)
+		{
+			var texture = new Texture2D(2, 2);
+			texture.LoadImage(btb);
+
+			var rect = new Rect(0, 0, texture.width, texture.height);
+			
+			return Sprite.Create(texture, rect, Vector2.one * 0.5f);
+		}
+
+		private static IEnumerable<UniTask<Sprite>> CreateImageLoadTasks(IEnumerable<string> p, TaskDelegate td)
+		{
+			return Enumerable.Select(p, td.Invoke);
+		}
+	}
+}
--- Assets/Scripts/Tester.cs
+++ Assets/Scripts/Tester.cs
@@ -1,16 +1,9 @@
-using Cysharp.Threading.Tasks;
-using HorangUnityLibrary.Foundation.Module;
 using HorangUnityLibrary.Managers.Module;
-using HorangUnityLibrary.Managers.RemoteMethodInterface;
-using HorangUnityLibrary.Managers.Static.Networking;
 using HorangUnityLibrary.Modules.StopwatchModule;
-using HorangUnityLibrary.Utilities;
 using UnityEngine;
 
 public class Tester : MonoBehaviour
 {
-	public SpriteRenderer sp;
-	
 	private StopwatchModule stopwatchModule;
 
 	private void Start()
@@ -18,40 +11,5 @@ public class Tester : MonoBehaviour
 		ModuleManager.Instance.RegisterModule(new StopwatchModule(ModuleManager.Instance));
 		stopwatchModule = ModuleManager.Instance.GetModule<StopwatchModule>(typeof(StopwatchModule));
 		stopwatchModule.ActiveModule();
-
-		var r = UnityWebRequestFactory.Get("https://webb.nasa.gov/content/webbLaunch/assets/images/images2023/jan-31-23-potm2301a-4k.jpg", ("authorization", "token"), ("x-device-id", "device"));
-
-		UniTask.Void(() =>
-			RequestManager.Send(r,
-				onSuccess: ByteToTexture,
-				onFailure: (code, msg) => $"code: {code}, msg: {msg}".ToLog(),
-				onProgress: percent => $"{r.uri.AbsoluteUri} - {percent * 100.0f}%".ToLog()));
-	}
-
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.F1))
-		{
-			var d = RmiManager.Instance.Run(typeof(Tester), "Bim");
-		}
-	}
-
-	[RMI]
-	private string Bim()
-	{
-		Log.Print("apsodfjapojpfoj");
-
-		return "asdfasdfasdf";
-	}
-
-	private void ByteToTexture(byte[] image)
-	{
-		var tex = new Texture2D(3840, 2160, TextureFormat.RGBA32, false);
-		tex.LoadRawTextureData(image);
-		tex.Apply();
-
-		var s = Sprite.Create(tex, new Rect(0.0f, 0.0f, 3840, 2160), Vector2.one * 0.5f);
-
-		sp.sprite = s;
 	}
 }
