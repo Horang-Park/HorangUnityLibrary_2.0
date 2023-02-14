9ca87ec25d58bc1ae6d826984a0e901bada912c6
HorangPark <horang_park@naver.com>
2023-02-15 08:59:08 +0900

    defined255 추가

--- Assets/Scripts/HorangUnityLibrary/Utilities/UnityExtensions/ColorExtension.cs
+++ Assets/Scripts/HorangUnityLibrary/Utilities/UnityExtensions/ColorExtension.cs
@@ -13,7 +13,8 @@ namespace Horang.HorangUnityLibrary.Utilities.UnityExtensions
 	
 	public static class ColorExtension
 	{
-		private const float Inverse255 = 1.0f / 255; // 0~255
+		private const int Defined255 = (2 << 8) - 1;
+		private const float Inverse255 = 1.0f / Defined255; // 0~255
 		private const int RequireHexLength = 6;
 
 		/// <summary>
@@ -23,29 +24,34 @@ namespace Horang.HorangUnityLibrary.Utilities.UnityExtensions
 		/// <returns>Converted color</returns>
 		public static Color Rgba256ToColor(ColorFormat256 color)
 		{
-			var rV = color.r * Inverse255;
-			var gV = color.g * Inverse255;
-			var bV = color.b * Inverse255;
-			var aV = color.a * Inverse255;
+			var rV = Mathf.Clamp(color.r * Inverse255, 0.0f, 1.0f);
+			var gV = Mathf.Clamp(color.g * Inverse255, 0.0f, 1.0f);
+			var bV = Mathf.Clamp(color.b * Inverse255, 0.0f, 1.0f);
+			var aV = Mathf.Clamp(color.a * Inverse255, 0.0f, 1.0f);
 			
 			return new Color(rV, gV, bV, aV);
 		}
 
 		/// <summary>
-		/// Unity color to RGBA 0~255
+		/// Unity color to RGBA 0~255.
 		/// </summary>
 		/// <param name="color">To convert color</param>
 		/// <returns>Converted color</returns>
 		public static ColorFormat256 ColorToRgba256(Color color)
 		{
-			var rV = (int)(color.r * 255);
-			var gV = (int)(color.g * 255);
-			var bV = (int)(color.b * 255);
-			var aV = (int)(color.a * 255);
+			var rV = (int)(color.r * Defined255);
+			var gV = (int)(color.g * Defined255);
+			var bV = (int)(color.b * Defined255);
+			var aV = (int)(color.a * Defined255);
 
 			return new ColorFormat256 { r = rV, g = gV, b = bV, a = aV };
 		}
 
+		/// <summary>
+		/// Web hex color to Unity color 0.0~1.0.
+		/// </summary>
+		/// <param name="colorHex">To change color hex. must be "#ffffff" or "ffffff"</param>
+		/// <returns>If valid colorHex, will return its color. otherwise return clear color</returns>
 		public static Color HexToColor(string colorHex)
 		{
 			colorHex = colorHex.Replace("#", string.Empty);
