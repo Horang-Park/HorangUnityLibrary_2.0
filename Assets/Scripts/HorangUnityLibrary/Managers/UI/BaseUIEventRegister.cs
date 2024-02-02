5d1912c1f3afe2d395cea58d03f477774f625ab4
HorangPark <horang_park@naver.com>
2024-02-02 10:25:12 +0900

    다중 Bind 방지 처리

--- Assets/Scripts/HorangUnityLibrary/Managers/UI/BaseUIEventRegister.cs
+++ Assets/Scripts/HorangUnityLibrary/Managers/UI/BaseUIEventRegister.cs
@@ -1,16 +1,29 @@
+using Horang.HorangUnityLibrary.Utilities;
+
 namespace Horang.HorangUnityLibrary.Managers.UI
 {
 	public abstract class BaseUIEventRegister
 	{
 		protected UIManager uiManager;
+
+		private bool bind;
 		
 		protected abstract void BindEvents();
 
 		public T Bind<T>(UIManager instance) where T : BaseUIEventRegister
 		{
+			if (bind)
+			{
+				Log.Print($"[{typeof(T)}] is already bound.", LogPriority.Warning);
+
+				return this as T;
+			}
+			
 			uiManager = instance;
 			
 			BindEvents();
+
+			bind = true;
 		
 			return this as T;
 		}
